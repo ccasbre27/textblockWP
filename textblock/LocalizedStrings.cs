29c3068473c6256dafd34d39ce0df57a98ec1a6a
Carlos Castro Brenes <ccasbre27@hotmail.com>
2015-11-23 23:03:28 +0000

    init

---
+++ textblock/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using TextBlock.Resources;
+
+namespace TextBlock
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
